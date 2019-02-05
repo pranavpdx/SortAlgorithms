c680d6558dea1de28cc5ccbdc6c6c3b7a7c2abb4
pranavpdx <pranavpdx@gmail.com>
2019-02-04 21:44:11 -0800

    Update Sort.java

--- src/Sort.java
+++ src/Sort.java
@@ -1,3 +1,8 @@
+/*
+ * This program Sorts 4 different files using multiple sorting methods and return the sorted numbers into an output file
+ * Author: Pranav Sharma
+ * Date: 10/11/18
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -6,6 +11,7 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Sort {
+	// creates starting variables for later use
 	Scanner console = new Scanner(System.in);
 	String input;
 	Scanner file;
@@ -13,9 +19,11 @@ public class Sort {
 	long startTime;
 
 	public Sort() {
+		// asks user to enter which file they would like to access
 		System.out.println("Enter a number for the input file");
-		System.out.println("1: input1.txt  2: input2.txt  3: input3.txt  4: input4.txt");
+		System.out.println("1: input1.txt  2: input2.txt  3: input3.txt  4:input4.txt");
 		input = console.nextLine();
+		// checks if input is valid
 		if (!input.equals("1") && !input.equals("2") && !input.equals("3") && !input.equals("4")) {
 			while (!input.equals("1") && !input.equals("2") && !input.equals("3") && !input.equals("4")) {
 				System.out.println("Enter 1, 2, 3 or 4");
@@ -23,43 +31,56 @@ public class Sort {
 			}
 		}
 		try {
+			// scanner to read the file
 			file = new Scanner(new File("input" + input + ".txt"));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			System.out.println("File not found");
 			System.exit(0);
 		}
+		// breaks the file into a string
 		String fileLine = file.nextLine();
+		// splits each number separated by a comma
 		String[] inputStringArray = fileLine.split(",");
 		inputArray = new int[inputStringArray.length];
+		// changes all the separated strings into integers
 		for (int i = 0; i < inputStringArray.length; i++) {
 			inputArray[i] = Integer.parseInt(inputStringArray[i]);
 			// System.out.println(inputArray[i]);
 		}
+		// asks which sort user would like to use
 		System.out.println("Enter a number for the sort you want to use");
 		System.out.println("1: Bubble   2: Selection  3: Table  4: Quick");
 		input = console.nextLine();
+		// Checks if user entered a valid input
 		if (!input.equals("1") && !input.equals("2") && !input.equals("3") && !input.equals("4")) {
 			while (!input.equals("1") && !input.equals("2") && !input.equals("3") && !input.equals("4")) {
 				System.out.println("Enter 1, 2, 3 or 4");
 				input = console.nextLine();
 			}
 		}
+		// starts timer to count how long it takes to sort the file
 		startTime = System.currentTimeMillis();
 		if (input.equals("1")) {
+			// runs Bubble Sort
 			inputArray = BubbleSort(inputArray);
 		} else if (input.equals("2")) {
+			// runs Selection Sort
 			inputArray = SelectionSort(inputArray);
 
 		} else if (input.equals("3")) {
+			// runs Table Sort
 			inputArray = TableSort(inputArray);
 		} else if (input.equals("4")) {
+			// runs Quick Sort
 			int low = 0;
-			int high = inputArray.length-1;
+			int high = inputArray.length - 1;
 			inputArray = QuickSort(inputArray, low, high);
 		}
+		// ends timer to count how long the sort took
 		long total = System.currentTimeMillis() - startTime;
 		System.out.println("Total time: " + total + " milliseconds");
+		// converts sorting integers into a file
 		PrintWriter pw;
 		try {
 			pw = new PrintWriter(new FileWriter(new File("output.txt")));
@@ -76,19 +97,26 @@ public class Sort {
 		}
 	}
 
+	// uses a partition point and moves all number under the point to the right and
+	// the larger ones to the right. Repeats process with new partition point until
+	// all are sorted
 	private int[] QuickSort(int[] array, int low, int high) {
-		int middle = low + (high - low) /2;
+		// finds partition point, in the middle
+		int middle = low + (high - low) / 2;
 		int pivot = array[middle];
-		int i = low, j = high;
+		int i = low; 
+		int j = high;
 		while (i <= j) {
+			// keeps checking until number is lower that pivot point
 			while (array[i] < pivot) {
 				i++;
 			}
- 
+			// keeps checking until number is higher that pivot point
 			while (array[j] > pivot) {
 				j--;
 			}
- 
+			// if the lower number is before the pivot point and the larger number is after
+			// the pivot point, then it swaps the numbers
 			if (i <= j) {
 				int temp = array[i];
 				array[i] = array[j];
@@ -97,19 +125,17 @@ public class Sort {
 				j--;
 			}
 		}
- 
+		// breaks the half with the lower numbers and quicksorts that until numbers are properly sorted 
 		if (low < j) {
 			QuickSort(array, low, j);
 		}
+		// breaks the half with the higher numbers and quicksorts that until numbers are properly sorted
 		if (high > i) {
 			QuickSort(array, i, high);
 		}
 		return array;
 	}
-
-		
 	
-
 	// counts how often number is seen, prints out how often
 	private int[] TableSort(int[] array) {
 		// TODO Auto-generated method stub
@@ -137,11 +163,13 @@ public class Sort {
 			int small = array[i];
 			int smallIndex = i;
 			for (int j = i; j < array.length; j++) {
+				// finds the smallest number
 				if (array[j] < small) {
 					small = array[j];
 					smallIndex = j;
 				}
 			}
+			// moves smallest number to the front
 			int temp = array[smallIndex];
 			array[smallIndex] = array[i];
 			array[i] = temp;
@@ -171,6 +199,5 @@ public class Sort {
 		// TODO Auto-generated method stub
 		new Sort();
 	}
-
 
 }
